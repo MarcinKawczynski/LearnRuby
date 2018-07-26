3da0b2e8d692f698afaa7bd8478ba84ca780ded3
Marcin Kawczyński <zielony.mowi@gmail.com>
2018-07-26 12:00:15 +0200

    First Commit

---
+++ 2018_07_24_HellowWorld/hw.rb
@@ -0,0 +1,17 @@
+puts "Hello World"
+hello = "Witaj świecie!" + "No tak..."
+puts hello
+
+#To jest koment lolz
+
+def say_hello
+  puts "Wilkomen Welt!"
+end
+
+def say_hello2(imie)
+  puts "Witaj " + imie
+end
+
+say_hello
+
+say_hello2("Marcin!")
